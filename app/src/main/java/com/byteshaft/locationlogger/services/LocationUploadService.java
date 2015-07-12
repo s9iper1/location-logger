45617f42a54d1b877d8280223993df9b8c723d86
Omer Akram <om26er@gmail.com>
2015-07-13 02:14:43 +0500

    Check internet availability and connectivity

--- app/src/main/java/com/byteshaft/locationlogger/services/LocationUploadService.java
+++ app/src/main/java/com/byteshaft/locationlogger/services/LocationUploadService.java
@@ -1,10 +1,17 @@
 package com.byteshaft.locationlogger.services;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import com.byteshaft.locationlogger.database.LocationDatabase;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -16,16 +23,42 @@ public class LocationUploadService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        // TODO: Check if internet is available and only proceed if we get a successful ping
-        LocationDatabase database = new LocationDatabase(getApplicationContext());
-        ArrayList<HashMap> allRecords = database.getAllRecords();
-        for (HashMap map : allRecords) {
-            String id = (String) map.get("unique_id");
-            String longitude = (String) map.get("longitude");
-            String latitude = (String) map.get("latitude");
-            String timeStamp = (String) map.get("time_stamp");
-            String userId = (String) map.get("user_id");
-            // TODO: Implement Upload here: if successful use database.deleteEntry()
+        if (isNetworkAvailable()) {
+            if (isInternetWorking()) {
+                LocationDatabase database = new LocationDatabase(getApplicationContext());
+                ArrayList<HashMap> allRecords = database.getAllRecords();
+                for (HashMap map : allRecords) {
+                    String id = (String) map.get("unique_id");
+                    String longitude = (String) map.get("longitude");
+                    String latitude = (String) map.get("latitude");
+                    String timeStamp = (String) map.get("time_stamp");
+                    String userId = (String) map.get("user_id");
+                    System.out.println("Internet is working: will upload" + id);
+                    // TODO: Implement Upload here: if successful use database.deleteEntry()
+                }
+            }
         }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private boolean isInternetWorking() {
+        boolean success = false;
+        try {
+            URL url = new URL("http://google.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(10000);
+            connection.connect();
+            success = connection.getResponseCode() == 200;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return success;
     }
 }
