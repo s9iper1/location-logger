41bc61ecb39d1585df40b9a45b8324363d4e3469
Omer Akram <om26er@gmail.com>
2015-07-12 22:52:17 +0500

    So that service does not crash when starting the App second time

--- app/src/main/java/com/byteshaft/locationlogger/MainActivity.java
+++ app/src/main/java/com/byteshaft/locationlogger/MainActivity.java
@@ -4,6 +4,9 @@ package com.byteshaft.locationlogger;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
 
 import com.byteshaft.locationlogger.database.LocationDatabase;
 import com.byteshaft.locationlogger.services.LocationService;
@@ -12,7 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,20 @@ public class MainActivity extends AppCompatActivity {
                     + map.get("user_id")
             );
         }
-        Intent service = new Intent(this, LocationService.class);
-        startService(service);
+        Switch toggle = (Switch) findViewById(R.id.switch_track_location);
+        toggle.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()) {
+            case R.id.switch_track_location:
+                Intent service = new Intent(this, LocationService.class);
+                if (b) {
+                    startService(service);
+                } else {
+                    stopService(service);
+                }
+        }
     }
 }
