f4ae4d757800e1420359973098bdfbf6955da5e5
Omer Akram <om26er@gmail.com>
2015-07-13 00:13:56 +0500

    Move code out to Helpers

--- app/src/main/java/com/byteshaft/locationlogger/utils/LocationHelpers.java
+++ app/src/main/java/com/byteshaft/locationlogger/utils/LocationHelpers.java
@@ -1,5 +1,13 @@
 package com.byteshaft.locationlogger.utils;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Handler;
+
 import com.google.android.gms.location.LocationRequest;
 
 import java.text.SimpleDateFormat;
@@ -7,16 +15,25 @@ import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
-public class LocationHelpers {
+public class LocationHelpers extends ContextWrapper {
 
-    public static LocationRequest createLocationRequest() {
+    private Handler mHandler;
+    private LocationRequest mLocationRequest;
+
+    public LocationHelpers(Context base) {
+        super(base);
+    }
+
+    public LocationRequest getLocationRequest() {
         long INTERVAL = 0;
         long FASTEST_INTERVAL = 0;
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(INTERVAL);
-        locationRequest.setFastestInterval(FASTEST_INTERVAL);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        return locationRequest;
+        if (mLocationRequest == null) {
+            mLocationRequest = new LocationRequest();
+            mLocationRequest.setInterval(INTERVAL);
+            mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        }
+        return mLocationRequest;
     }
 
     public static String getTimeStamp() {
@@ -26,5 +43,28 @@ public class LocationHelpers {
                 new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.UK);
         simpleDateFormat.setTimeZone(timeZone);
         return simpleDateFormat.format(calendar.getTime());
+    }
+
+    public static String getLongitudeAsString(Location location) {
+        return String.valueOf(location.getLongitude());
+    }
+
+    public static String getLatitudeAsString(Location location) {
+        return String.valueOf(location.getLatitude());
+    }
+
+    public void setLocationAlarm(int time) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent intent = new Intent("com.byteshaft.LOCATION_ALARM");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + time, pendingIntent);
+    }
+
+    public Handler getHandler() {
+        if (mHandler == null) {
+            mHandler = new Handler();
+        }
+        return mHandler;
     }
 }
